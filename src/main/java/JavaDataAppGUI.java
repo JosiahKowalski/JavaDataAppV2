f4e09bf2203a4d5ecc2f70af613fe453989bc5ee
josiah <josiah.kowalski724@gmail.com>
2021-10-15 16:48:41 -0400

    Refined the GUI and added a dropdown menu for currency.

--- src/main/java/JavaDataAppGUI.java
+++ src/main/java/JavaDataAppGUI.java
@@ -1,9 +1,10 @@
-
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 
 public class JavaDataAppGUI extends JFrame {
@@ -12,6 +13,7 @@ public class JavaDataAppGUI extends JFrame {
     private JLabel ticker;
     private JLabel tickerOutput;
     private JPanel DataApp;
+    private JComboBox currencyBox;
 
     public JavaDataAppGUI() {
         searchButton.addActionListener(new ActionListener() {
@@ -23,14 +25,29 @@ public class JavaDataAppGUI extends JFrame {
                 }
             }
         });
+        tickerInput.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode()==KeyEvent.VK_ENTER) {
+                    try {
+                        onSearch();
+                    } catch (UnirestException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 
     private void onSearch() throws UnirestException {
         // add your code here
         String ticker = tickerInput.getText();
+        String currency = String.valueOf(currencyBox.getSelectedItem());
         JavaDataAppV2 data = new JavaDataAppV2();
-        String URL = data.buildURL(ticker.toUpperCase());
-        tickerOutput.setText(data.handleURL(URL));
+        String URL = data.buildURL(ticker.toUpperCase(), currency.toUpperCase());
+        String output = data.handleURL(URL);
+        if (output == null) tickerOutput.setText("This ticker doesn't seem to be in our records.    Try another.");
+        else tickerOutput.setText(output);
     }
 
     public static void main(String[] args) {
--- src/main/java/JavaDataAppV2.java
+++ src/main/java/JavaDataAppV2.java
@@ -5,8 +5,6 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONObject;
 
-import java.util.Scanner;
-
 /*
  * possibly use the JSON Deserializer
  * https://www.tutorialspoint.com/how-to-deserialize-a-json-to-java-object-using-the-flexjson-in-java
@@ -19,25 +17,26 @@ public class JavaDataAppV2 {
     }
 
     //Receives user input
-    public String input() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter any stock ticker or press x to exit: ");
-        ticker = in.next();
-        if (ticker.equals("x")) {
-            return "";
-        }
-        ticker = ticker.toUpperCase();
-        return buildURL(ticker);
-    }
+//    public String input() {
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Enter any stock ticker or press x to exit: ");
+//        ticker = in.next();
+//        if (ticker.equals("x")) {
+//            return "";
+//        }
+//        ticker = ticker.toUpperCase();
+//        return buildURL(ticker);
+//    }
 
     //Generates the URL using user input
-    public String buildURL(String ticker) {
-        return "https://rest.coinapi.io/v1/exchangerate/"+ticker+"/USD";
+    public String buildURL(String ticker, String currency) {
+        return "https://rest.coinapi.io/v1/exchangerate/" + ticker + "/" + currency;
     }
 
     public String[] getDateTime(JSONObject stock) {
         String dateTime = stock.getString("time");
-        String dateTimeStr = dateTime.replaceAll("^\"|\"$", "");;
+        String dateTimeStr = dateTime.replaceAll("^\"|\"$", "");
+        ;
         String[] dateTimeArr = dateTimeStr.split("T");
         dateTimeArr[1] = dateTimeArr[1].replace(".0000000Z", "");
         return dateTimeArr;
@@ -50,7 +49,7 @@ public class JavaDataAppV2 {
         String quote = stock.getString("asset_id_quote");
         double rate = stock.getDouble("rate");
 
-        return String.format("Date: %s\nTime: %s\nTicker: %s\nQuote: %s\nPrice: %f.3",dateTime[0], dateTime[1], base, quote, rate);
+        return String.format("Date: %s  Time: %s  Ticker: %s  Quote: %s  Price: %f", dateTime[0], dateTime[1], base, quote, rate);
 //        System.out.println(string);
 //        System.out.println("Price: "+rate);
     }
@@ -72,20 +71,20 @@ public class JavaDataAppV2 {
         }
     }
 
-    public static void main(String[] args) throws UnirestException {
-        JavaDataAppV2 data = new JavaDataAppV2();
-        while (true) {
-            String my_url = data.input();
-            if (my_url.equals("")) {
-                System.out.println("Exiting program");
-                break;
-            }
-            data.handleURL(my_url);
-        }
-
+//    public static void main(String[] args) throws UnirestException {
+//        JavaDataAppV2 data = new JavaDataAppV2();
+//        while (true) {
+//            String my_url = data.input();
+//            if (my_url.equals("")) {
+//                System.out.println("Exiting program");
+//                break;
+//            }
+//            data.handleURL(my_url);
+//        }
+//}
         //String URL = "https://rest.coinapi.io/v1/exchangerate/"+ticker+"/USD?apikey=E45F127F-46D7-46C3-8C5E-4A96413B319C";
         //String URL = "https://rest.coinapi.io/v1/exchangerate/USD?apikey=E45F127F-46D7-46C3-8C5E-4A96413B319C&invert=true&output_format=csv";
         //Backup plan if URL doesnt work, use this CSV
         //String URL = "https://rest.coinapi.io/v1/exchangerate/USD?apikey=E45F127F-46D7-46C3-8C5E-4A96413B319C";
-    }
+
 }
